776381ef5cb970748e7bb0c727f2a74d537b69c4
Abigail <abigailsalcedo798@gmail.com>
2023-11-05 17:48:25 -0500

    unu cors

--- DBP/demo/src/main/java/com/example/demo/usuario/config/SecurityConfiguration.java
+++ DBP/demo/src/main/java/com/example/demo/usuario/config/SecurityConfiguration.java
@@ -16,6 +16,8 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import static org.springframework.security.config.http.SessionCreationPolicy.STATELESS;
 import static org.springframework.security.web.util.matcher.AntPathRequestMatcher.antMatcher;
@@ -41,6 +43,18 @@ public class SecurityConfiguration {
                 .authenticationProvider(authenticationProvider())
                 .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
         return http.build();
+    }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer(){
+        return new WebMvcConfigurer(){
+                @Override
+                public void addCorsMappings(CorsRegistry registry){
+                registry.addMapping("/**");
+        }
+    };
+
+       
     }
 
     @Bean
