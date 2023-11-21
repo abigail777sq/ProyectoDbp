9cbb7c2fa9d91aefb29b061389d467855cedfb69
Abigail <abigailsalcedo798@gmail.com>
2023-11-21 00:30:41 -0500

    uwu

--- DBP/demo/src/main/java/com/example/demo/MesaEnReserva/MesaEnReservaController.java
+++ DBP/demo/src/main/java/com/example/demo/MesaEnReserva/MesaEnReservaController.java
@@ -2,6 +2,7 @@ package com.example.demo.MesaEnReserva;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.annotation.Secured;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -13,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class MesaEnReservaController {
     @Autowired
     MesaEnReservaService mesaEnReservaService;
+     @Secured({"ADMIN", "USER"})
     @PostMapping
     public ResponseEntity<String> ReservarMesasEnReserva(@RequestParam Long MesaId, @RequestParam Long reservaId){
         try {
