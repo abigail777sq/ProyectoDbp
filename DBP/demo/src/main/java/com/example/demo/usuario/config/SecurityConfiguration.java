b2e46aaf9b0e0dfaa021c2721ae38f5a2302f200
Abigail <abigailsalcedo798@gmail.com>
2023-11-22 19:44:18 -0500

    unu

--- DBP/demo/src/main/java/com/example/demo/usuario/config/SecurityConfiguration.java
+++ DBP/demo/src/main/java/com/example/demo/usuario/config/SecurityConfiguration.java
@@ -36,7 +36,7 @@ public class SecurityConfiguration {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.csrf(AbstractHttpConfigurer::disable)
-                .authorizeHttpRequests(auth -> auth.requestMatchers(antMatcher("*")).permitAll().anyRequest().authenticated())
+                .authorizeHttpRequests(auth -> auth.requestMatchers(antMatcher("/api/auth/**")).permitAll().anyRequest().authenticated())
                 .sessionManagement(manager -> manager.sessionCreationPolicy(STATELESS))
                 .authenticationProvider(authenticationProvider())
                 .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
